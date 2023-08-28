81641428d3bc002a0ff55a27301d5303557f5ada
emanoelimadalosso <emanoeli.madalosso@softfocus.com.br>
2023-08-28 20:37:33 -0300

    ajuste settings

--- livraria/settings.py
+++ livraria/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'livraria.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
+        'NAME': config('POSTGRES_DATABASE'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
         'PORT': config('DATABASE_PORT', default='5432'),
     }
 }
